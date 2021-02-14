d2cd2ab065abda866e30fe9d021c2659faf875bb
syllomex <leonardojdossantos@gmail.com>
2021-02-14 14:15:37 -0300

    chore: clean up project files

---
+++ pages/index.tsx
@@ -0,0 +1,14 @@
+export default function Home() {
+  return (
+    <div
+      style={{
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        height: '100vh',
+      }}
+    >
+      <h1>BirdBox Nome Provisório</h1>
+    </div>
+  );
+}
